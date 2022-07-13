90f60c48b02af16855d4f233cbb5b7e81fd153bf
João Pedro Bastos <jpedrosantosb@gmail.com>
2022-07-13 19:43:41 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -13,7 +17,14 @@ public class SaleService {
 	@Autowired
 	private SaleRepository rep;
 	
-	public List<Sale> findSales() {
-		return rep.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable peageble) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		
+		return rep.findSales(min, max, peageble);
 	}
 }
